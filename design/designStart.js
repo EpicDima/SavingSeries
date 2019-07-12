ae11a93a64d4bb9df1fced3cdb901a39d58609f0
Dima <dima2-8@bk.ru>
2019-07-12 05:00:31 +0300

    Ещё немного добавлений к первоначальному дизайну

--- design/designStart.js
+++ design/designStart.js
@@ -1,12 +1,20 @@
 function plusClick() {
-    //alert("add")
-    let element = document.getElementById("add-container")
-    element.style.display = (element.style.display == "block") ? "none" : "block"
+    //document.getElementById("sort-types").style.display = "none"
+    //let element = document.getElementById("add-container")
+    //element.style.display = (element.style.display == "block") ? "none" : "block"
+
+    
+    let div = document.createElement("div")
+    div.style.height = "70px"
+    div.style.margin = "5px"
+    div.style.background = "gold"
+    document.getElementById("content-container").appendChild(div)
+    div.scrollIntoView()
 }
 
 
 function sortClick() {
-    //alert("sort")
+    document.getElementById("add-container").style.display = "none"
     let element = document.getElementById("sort-types")
     element.style.display = (element.style.display == "block") ? "none" : "block"
 }
