28f2cb15faee09d6d4669bd6af1a7c00f06aa43b
Dima <dima2-8@bk.ru>
2019-07-20 03:24:18 +0300

    Начало уведомлений

---
+++ worker.js
@@ -0,0 +1,12 @@
+if ('serviceWorker' in navigator) {
+    // Регистрация service worker-а, расположенного в корне сайта
+    // за счет использования дефолтного scope (не указывая его)
+    navigator.serviceWorker.register('worker.js').then(function(registration) {
+        console.log('Service worker зарегистрирован:', registration);
+    }).catch(function(error) {
+        console.log('Ошибка при регистрации service worker-а:', error);
+    });
+} else {
+// Текущий браузер не поддерживает service worker-ы.
+console.log('Текущий браузер не поддерживает service worker-ы');
+}
