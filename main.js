fa780bee45fadd777c6efb24b7786d22179a7872
Dima <dima2-8@bk.ru>
2020-01-12 05:59:44 +0300

    Сделана основная вёрстка, работает отображение сериалов

--- main.js
+++ main.js
@@ -4,53 +4,27 @@ var seriesList = []   // список объектов сериалов
 var cardList = []     // список элементов div
 
 // Виды сортировок
-const SORT_TYPES = {NONE: 0,
-                    NAME_UP: 1, NAME_DOWN: 2,
-                    SEASON_UP: 3, SEASON_DOWN: 4,
-                    EPISODE_UP: 5, EPISODE_DOWN: 6,
-                    DATE_UP: 7, DATE_DOWN: 8}
+const SORT_TYPES = {ID_UP: 1, ID_DOWN: 2,
+                    NAME_UP: 3, NAME_DOWN: 4,
+                    SEASON_UP: 5, SEASON_DOWN: 6,
+                    EPISODE_UP: 7, EPISODE_DOWN: 8,
+                    DATE_UP: 9, DATE_DOWN: 10}
 
 // Текущий используемый вид сортировки
-var currentSortType = SORT_TYPES.NONE
+var currentSortType = SORT_TYPES.ID_UP
 
 
 // База данных Indexed DB
 var database;
 
-
-
-// Основные неизменяемые элементы пользовательского интерфейса
-// Кнопки
-const settingsButton = document.getElementById("settings")
-const plusButton = document.getElementById("plus")
-const sortButton = document.getElementById("sort")
-
-const addAcceptButton = document.getElementById("add-accept")
-
-// Показывающиеся при нажатии элементы
-const settingsMenu = document.getElementById("settings-menu")
-const backgroundAddContainer = document.getElementById("background-add-container")
-const addContainer = document.getElementById("add-container")
-const addContainerError = document.getElementById("error")
-const sortTypes = document.getElementById("sort-types")
-
 // Элемент для хранения карточек серий
-const contentContainer = document.getElementById("content-container")
-
-// Поля ввода для добавления
-const nameInput = document.getElementById("name-input")
-const seasonInput = document.getElementById("season-input")
-const episodeInput = document.getElementById("episode-input")
-const dateInput = document.getElementById("date-input")
-const siteInput = document.getElementById("site-input")
+const contentContainer = document.getElementById("content")
 
 // Поисковая строка
-const searchInput = document.getElementById("search-input")
+const search = document.getElementById("search")
 
 
 
-onClickOutside()
-resetAddInputs()
 connectDB(initialize)
 
 
@@ -72,7 +46,7 @@ function connectDB(func) {
             connectDB(func);
         } else {
             indexedDB.deleteDatabase("SavingSeriesDb");
-            document.getElementsByTagName("body")[0].remove()
+            document.getElementsByTagName("html")[0].remove()
         }
     }
 }
@@ -98,60 +72,7 @@ function initialize(db) {
         window.scrollTo(0, 0)
     }
 
-    searchInput.value = ""
-}
-
-
-function onClickOutside() {
-    document.addEventListener("click", event => {
-        let target = event.target
-        
-        if (settingsButton.contains(target)) {
-            settingsMenu.style.display = (settingsMenu.style.display == "block") ? "none" : "block"
-        } else {
-            settingsMenu.style.display = "none"
-        }
-    
-        if (plusButton.contains(target)) {
-            backgroundAddContainer.style.display = (backgroundAddContainer.style.display == "flex") ? "none" : "flex"
-        } else if (!addContainer.contains(target)) {
-            backgroundAddContainer.style.display = "none"
-        }
-    
-        if (sortButton.contains(target)) {
-            if (sortTypes.style.display == "block") {
-                sortTypes.style.display = "none"
-            } else {
-                if (currentSortType != SORT_TYPES.NONE) {
-                    let element = document.getElementById("sort-type-" + currentSortType)
-                    element.style.backgroundColor = "#ee0000"
-                }
-                sortTypes.style.display = "block"
-                
-            }
-        } else {
-            sortTypes.style.display = "none"
-            for (let i = 1; i <= 8; i++) {
-                let element = document.getElementById("sort-type-" + i)
-                element.style.backgroundColor = "#272727"
-            }
-        }
-
-        let changeContainer = document.getElementById("change-container")
-        if (changeContainer != null) {
-            for (let i = 0; i < cardList.length; i++) {
-                if (cardList[i].contains(target)) {
-                    return;
-                }
-            }
-            changeContainer.remove()
-        }
-    });
-}
-
-
-function closeAddContainer() {
-    backgroundAddContainer.style.display = "none"
+    search.value = ""
 }
 
 
@@ -254,110 +175,6 @@ function nameExistenceValidator() {
 }
 
 
-function addSeriesValidator(series, callback) {
-    callback("")
-    if (!nameValidator(series, callback)) {
-        return null;
-    } else if (!seasonValidator(series, callback)) {
-        return null;
-    } else if (!episodeValidator(series, callback)) {
-        return null;
-    } else if (!dateValidator(series, callback)) {
-        return null;
-    } else if (!siteValidator(series, callback)) {
-        return null;
-    }
-    return series;
-}
-
-
-function nameValidator(series, callback) {
-    series.name = series.name.trim()
-    if (series.name.length == 0) {
-        callback("Некорректное название")
-        return false;
-    } 
-    else {
-        for (let i = 0; i < seriesList.length; i++) {
-            if (seriesList[i].name == series.name) {
-                callback("Сериал с таким названием уже существует")
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-
-function seasonValidator(series, callback) {
-    if (series.season.length == 0) {
-        callback("Некорректное число сезонов")
-        return false;
-    } else {
-        series.season = Number.parseInt(series.season)
-        if (isNaN(series.season)) {
-            callback("Некорректное число сезонов")
-            return false;
-        } else if (series.season <= 0) {
-            callback("Число сезонов не может быть меньшим либо равным нуля")
-            return false;
-        } else if (series.season > 50) {
-            callback("Число сезонов не может быть больше 50")
-            return false;
-        }
-    }
-    return true;
-}
-
-
-function episodeValidator(series, callback) {
-    if (series.episode.length == 0) {
-        callback("Некорректное число серий")
-        return false;
-    } else {
-        series.episode = Number.parseInt(series.episode)
-        if (isNaN(series.episode)) {
-            callback("Некорректное число серий")
-            return false;
-        } else if (series.episode <= 0) {
-            callback("Число серий не может быть меньшим либо равным нуля")
-            return false;
-        } else if (series.episode > 50000) {
-            callback("Число сезонов не может быть больше 50000")
-            return false;
-        }
-    }
-    return true;
-}
-
-
-function dateValidator(series, callback) {
-    if (series.date != "") {
-        series.date = new Date(series.date);
-        if (isNaN(series.date)) {
-            callback("Некорректная дата")
-            return false;
-        }
-    }
-    return true;
-}
-
-
-function siteValidator(series, callback) {
-    if (series.site != "") {
-        if (series.site.length == 0) {
-            callback("Некорректная ссылка на сайт")
-            return false;
-        }
-    }
-    return true;
-}
-
-
-function showAddError(message) {
-    addContainerError.innerText = message;
-}
-
 
 function addNew() {
     let series = addSeriesValidator(getSeriesValuesFromInput(), showAddError)
@@ -381,98 +198,31 @@ function addNew() {
 
 
 function addSeriesToList(series) {
-    let card = createCard(series)
+    let card = document.createElement("div");
+    card.className = "card";
+    card.id = "id" + series.id;
+    card.innerHTML = createCard(series);
     cardList.push(card)
     contentContainer.appendChild(card)
 }
 
-
 function createCard(series)
 {
-    let card = document.createElement("div")
-    card.className = "card"
-    card.onclick = openChangeContainer
-
-    let cross = document.createElement("div")
-    cross.className = "card-cross"
-    cross.innerHTML = "<svg class=\"card-cross-svg\" width=\"28\" height=\"28\" viewBox=\"0 0 24 24\"><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z\"/><path d=\"M0 0h24v24H0z\" fill=\"none\"/></svg>"
-    cross.onclick = remove
-
-    let name = document.createElement("div")
-    name.className = "card-name"
-    name.innerText = series.name
-
-    let table = document.createElement("table")
-    table.className = "card-table"
-
-    let tr1 = document.createElement("tr")
-    let tr2 = document.createElement("tr")
-
-    let td11 = document.createElement("td")
-    td11.innerText = "Сезон"
-    let td12 = document.createElement("td")
-    td12.innerText = "Серия"
-
-    let td21 = document.createElement("td")
-    td21.innerText = series.season
-    let td22 = document.createElement("td")
-    td22.innerText = series.episode
-
-    tr1.appendChild(td11)
-    tr1.appendChild(td12)
-
-    tr2.appendChild(td21)
-    tr2.appendChild(td22)
-
-    let td13 = document.createElement("td")
-    let td23 = document.createElement("td")
-    if (series.date != "") {
-        td13.innerText = "Дата"
-        td23.innerText = series.date.toLocaleDateString("ru", {
-            year: "numeric",
-            month: "long",
-            day: "numeric"
-        });
-    }
-    tr1.appendChild(td13)
-    tr2.appendChild(td23)
-
-    let td14 = document.createElement("td")
-    let td24 = document.createElement("td")
-    if (series.site != "") {
-        td14.innerText = "Ссылка"
-        
-        let a = document.createElement("a")
-        a.href = series.site
-        a.onclick = (event) => {
-            event.stopPropagation()
+    let link = "";
+    if (series.site !== "") {
+        let a = document.createElement("a");
+        a.href = series.site;
+        if (a.host !== "") {
+            link = a.host;
         }
-        
-        if (a.host == "") {
-            a.innerText = a.href
-        } else {
-            a.innerText = a.host
-        }
-        
-        a.target = "_blank"
-        td24.appendChild(a)
     }
-    tr1.appendChild(td14)
-    tr2.appendChild(td24)
-
-    table.appendChild(tr1)   
-    table.appendChild(tr2)
-
-    card.appendChild(cross)
-    card.appendChild(name)
-    card.appendChild(table)
-
-    return card
+    
+    return `<div data-toggle="collapse" href="#change${series.id}" aria-expanded="false" aria-controls="change${series.id}"><div class="card-header row"><h4 class="col">${series.name}</h4> <button type="button" class="close col-1" onclick="event.stopPropagation(); deleteSeries(${series.id})"><span>&times;</span></button></div><div class="card-body"><div class="row"><div class="col">Сезон</div><div class="col">Серия</div><div class="col">${series.date === "" ? "" : "Дата"}</div><div class="col">${series.site === "" ? "" : "Сайт"}</div></div><div class="row"><div class="col">${series.season}</div><div class="col">${series.episode}</div><div class="col">${series.date !== "" ? series.date.toLocaleDateString("ru", {year: "numeric", month: "long", day: "numeric"}) : ""}</div><div class="col"><a href="${series.site}" target="_blank" onclick="event.stopPropagation();">${link}</a></div></div></div></div><div class="collapse" id="change${series.id}" onclick="event.stopPropagation();"><br/><form name="changeForm${series.id}"><div class="form-group row"><label class="col col-form-label text-left ml-3">Сезон</label><div class="col mr-3"><input class="form-control" id="inputSeason" name="season" value="${series.season}" type="number" value="1" min="1" max="50" required/></div></div><div class="form-group row"><label class="col col-form-label text-left ml-3">Серия</label><div class="col mr-3"><input class="form-control" id="inputEpisode" name="episode" value="${series.episode}" type="number" value="1" min="1" max="50000" required/></div></div><div class="form-group row"><label class="col col-form-label text-left ml-3">Дата</label><div class="col mr-3"><input class="form-control" id="inputDate" name="date" type="date" value="${series.date === "" ? "" : new Date(series.date).toISOString().split("T")[0]}" optional/></div></div><div class="form-group row"><label class="col col-form-label text-left ml-3">Сайт</label><div class="col mr-3"><input class="form-control" id="inputSite" name="site" type="url" value="${series.site}" optional/></div></div><div class="form-group row justify-content-center"><button type="submit" class="btn btn-primary col-sm-5" onclick="changeSeries(${series.id})">Подтвердить</button></div></form></div>`;
 }
 
 
 function searchSeries() {
-    let substr = searchInput.value.trim().toLowerCase()
+    let substr = search.value.trim().toLowerCase()
     if (substr.length == 0) {
         cardList.forEach(card => {
             card.style.display = "block"
@@ -631,6 +381,7 @@ function openChangeContainer(event) {
         input3.value = date.toISOString().split("T")[0];
     }
     li3.appendChild(input3)
+
 
     let li4 = document.createElement("li")
     li4.innerText = "Сайт"
